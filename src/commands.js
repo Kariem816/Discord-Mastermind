21d9885dac828358f0de384b18e8b1ed72553259
Bumble <63314839+Kariem816@users.noreply.github.com>
2023-02-22 19:19:44 +0200

    Installing Commands on each guild

--- src/commands.js
+++ src/commands.js
@@ -1,13 +1,27 @@
 import { capitalize, DiscordRequest } from './utils.js';
 
-export async function HasGuildCommands(appId, guildId, commands) {
-  if (guildId === '' || appId === '') return;
+export async function HasGuildCommands(appId, commands) {
+  if (appId === '') return;
 
-  commands.forEach((c) => HasGuildCommand(appId, guildId, c));
+  // GET guilds
+  const guilds = await GetGuilds(appId);
+  guilds.forEach(async (guild) => {
+    // GET commands
+    commands.forEach(async (command) => {
+      await HasGuildCommand(appId, guild['id'], command, guild['name']);
+    });
+  });
+}
+
+// Gets all guilds the bot is in
+async function GetGuilds() {
+  // API endpoint to get guilds
+  const endpoint = 'users/@me/guilds';
+  return await DiscordRequest(endpoint, { method: 'GET' });
 }
 
 // Checks for a command
-async function HasGuildCommand(appId, guildId, command) {
+async function HasGuildCommand(appId, guildId, command, guildName = "") {
   // API endpoint to get and post guild commands
   const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
 
@@ -19,10 +33,10 @@ async function HasGuildCommand(appId, guildId, command) {
       const installedNames = data.map((c) => c['name']);
       // This is just matching on the name, so it's not good for updates
       if (!installedNames.includes(command['name'])) {
-        console.log(`Installing "${command['name']}"`);
-        InstallGuildCommand(appId, guildId, command);
+        console.log(`Installing "${command['name']}" command in ${guildName}`);
+        InstallGuildCommand(appId, guildId, command,);
       } else {
-        console.log(`"${command['name']}" command already installed`);
+        console.log(`"${command['name']}" command already installed in ${guildName}`);
       }
     }
   } catch (err) {
@@ -101,12 +115,6 @@ export const PLAY_COMMAND = {
       type: 4,
       name: 'length',
       description: 'Pick your length',
-      required: false,
-    },
-    {
-      type: 5,
-      name: 'random',
-      description: 'Let the bot pick a random word',
       required: false,
     }
   ],
